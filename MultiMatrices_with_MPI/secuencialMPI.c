65e001669e06f4dbbf69a2fce179782d8a73df0f
Sanss <santi-2000-12@hotmail.com>
2023-06-15 22:17:49 -0500

    ultimoporfa

--- MultiMatrices_with_MPI/secuencialMPI.c
+++ MultiMatrices_with_MPI/secuencialMPI.c
@@ -3,10 +3,9 @@
 #include <time.h>
 #include <mpi.h>
 
-void calcular_area_circulo(int **result, int n);
-void llenar_matrices(int **matrix1, int **matrix2, int n);
-void memory_storage(int ***matrix, int n);
-void release_memory(int **matrix, int n);
+void llenar_matrices(int *matrix1, int *matrix2, int n);
+void memory_storage(int **matrix, int n);
+void release_memory(int *matrix);
 
 int main(int argc, char *argv[]) {
     int rank, size;
@@ -18,7 +17,7 @@ int main(int argc, char *argv[]) {
     clock_t inicio, fin;
     double tiempo;
 
-    inicio = clock(); // Tomamos el tiempo de inicio
+    inicio = clock(); // Tomar el tiempo de inicio
 
     if (argc != 3) {
         if (rank == 0) {
@@ -33,9 +32,9 @@ int main(int argc, char *argv[]) {
     int i, j, k;
 
     int local_n = n / size; // Tamaño local de las matrices
-    int **local_matrix1 = NULL;
-    int **local_matrix2 = NULL;
-    int **local_result = NULL;
+    int *local_matrix1 = NULL;
+    int *local_matrix2 = NULL;
+    int *local_result = NULL;
 
     memory_storage(&local_matrix1, local_n * n);
     memory_storage(&local_matrix2, local_n * n);
@@ -48,36 +47,30 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < local_n; i++) {
         for (j = 0; j < n; j++) {
             for (k = 0; k < n; k++) {
-                local_result[i*n][j] += local_matrix1[i*n][k] * local_matrix2[k][j];
+                local_result[i * n + j] += local_matrix1[i * n + k] * local_matrix2[k * n + j];
             }
         }
     }
 
     // Recopilar los resultados locales en el proceso raíz
-    int **result = NULL;
+    int *result = NULL;
     if (rank == 0) {
-        result = (int **)malloc(n * sizeof(int *));
-        memory_storage(&result, n);
+        result = (int *)malloc(n * n * sizeof(int));
     }
-    MPI_Gather(local_result[0], local_n * n, MPI_INT, result[0], local_n * n, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Imprimir la matriz resultado en el proceso raíz
-    if (rank == 0 && impr > 0) {
-        calcular_area_circulo(result, n);
-    }
+    MPI_Gather(local_result, local_n * n, MPI_INT, result, local_n * n, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Liberar memoria en cada proceso
-    release_memory(local_matrix1, local_n);
-    release_memory(local_matrix2, local_n);
-    release_memory(local_result, local_n);
+    release_memory(local_matrix1);
+    release_memory(local_matrix2);
+    release_memory(local_result);
 
     // Liberar memoria en el proceso raíz
     if (rank == 0) {
-        release_memory(result, n);
+        release_memory(result);
     }
 
-    fin = clock(); // Tomamos el tiempo de finalización
-    tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC; // Calculamos el tiempo transcurrido
+    fin = clock(); // Tomar el tiempo de finalización
+    tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC; // Calcular el tiempo transcurrido
 
     if (rank == 0) {
         printf("Tiempo transcurrido: %f segundos\n", tiempo);
@@ -87,49 +80,21 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-//------------------------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------
+void llenar_matrices(int *matrix1, int *matrix2, int n) {
+    int i;
 
-// Imprimir la matriz resultado
-void calcular_area_circulo(int **result, int n) {
-    int i, j;
-
-    printf("La matriz resultado es:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d ", result[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void llenar_matrices(int **matrix1, int **matrix2, int n) {
-    int i, j;
-
-    // Llenar la matriz con valores aleatorios
+    // Llenar las matrices con valores aleatorios
     srand(time(NULL)); // Inicializar la semilla para generar valores aleatorios
     for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            matrix1[i][j] = rand() % 100;
-            matrix2[i][j] = rand() % 100;
-        }
+        matrix1[i] = rand() % 100;
+        matrix2[i] = rand() % 100;
     }
 }
 
-void memory_storage(int ***matrix, int n) {
-    *matrix = (int **)malloc(n * sizeof(int *));
-    int i;
-    for (i = 0; i < n; i++) {
-        (*matrix)[i] = (int *)malloc(n * sizeof(int));
-    }
+void memory_storage(int **matrix, int n) {
+    *matrix = (int *)malloc(n * sizeof(int));
 }
 
-void release_memory(int **matrix, int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-        free(matrix[i]);
-    }
+void release_memory(int *matrix) {
     free(matrix);
 }
